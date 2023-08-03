3a2b1111575c70a9732dcb2f983eedae28a4abd7
alanfedrizzi <alanfedrizzi@gmail.com>
2023-08-03 17:22:14 +0200

    testando fetch wordpress rest api

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 
 import { Todo } from '../todo.model';
 import { TodoService } from '../todo.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -11,12 +12,35 @@ import { TodoService } from '../todo.service';
 export class HomePage implements OnInit {
   @Input() todos: Todo[];
 
-  constructor(private todoService: TodoService) {}
+  constructor(private todoService: TodoService, private http: HttpClient) {}
 
   ngOnInit() {
     this.todos = this.todoService.getTodos();
     this.todoService.todosUpdated.subscribe((todos: Todo[]) => {
       this.todos = todos;
     });
+  }
+
+  fetchData(section: string) {
+    const test = this.http
+      .get<{
+        id: number;
+        title: string;
+        content: string;
+        slug: string;
+        order: number;
+        teste: { subteste_1: string; subteste_2: string };
+      }>(
+        `http://institucional-medgrupo-wp.local/wp-json/templarios/home/${section}`
+      )
+      .subscribe((data) => {
+        console.log(data);
+        console.log(data.id);
+        console.log(data.title);
+        console.log(data.content);
+        console.log(data.slug);
+        console.log(data.order);
+        console.log(data.teste);
+      });
   }
 }
